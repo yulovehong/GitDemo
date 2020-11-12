2f9542b6133ed7b8da745611d0629bfa250baf96
yulovehong <767038471@qq.com>
2020-11-12 14:12:43 +0800

    add:implement a viewmodel 添加了数据防丢失

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -6,24 +6,27 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
-    private int number = 0;
     Button buttonadd, buttonsubtract;
     TextView textView;
+    MyViewModel myViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        textView.setText(String.valueOf(myViewModel.number));
         buttonadd = findViewById(R.id.button);
         buttonsubtract = findViewById(R.id.button2);
         textView = findViewById(R.id.text);
         buttonadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText(String.valueOf(number++));
+                textView.setText(String.valueOf(myViewModel.number++));
             }
         });
     }
---
+++ app/src/main/java/com/example/gitdemo/MyViewModel.java
@@ -0,0 +1,7 @@
+package com.example.gitdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    public int number=0;
+}
