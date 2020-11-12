ef7489ea87ae33ad7b84e22cab4e68a9565d3d31
yulovehong <767038471@qq.com>
2020-11-12 14:19:28 +0800

    add:implement a item function 添加了项目功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.gitdemo;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.mymenu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.clear) {
+            textView.setText(String.valueOf(0));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
