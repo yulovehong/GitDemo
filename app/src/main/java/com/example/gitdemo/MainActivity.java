b353b590f38c262857cfc893c3ac22ebfe3e4829
yulovehong <767038471@qq.com>
2020-11-12 13:21:46 +0800

    add:implement a add function 添加了一个增加按钮功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.gitdemo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private int number = 0;
+    Button buttonadd, buttonsubtract;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonadd = findViewById(R.id.button);
+        buttonsubtract = findViewById(R.id.button2);
+        textView = findViewById(R.id.text);
+        buttonadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(String.valueOf(number++));
+            }
+        });
     }
 }
